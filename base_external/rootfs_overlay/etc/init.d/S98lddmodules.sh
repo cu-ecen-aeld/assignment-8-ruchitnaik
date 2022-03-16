aa8aca9354f34bf06d71228a7c517d4e5ecf2947
ruchitnaik <ruchit.naik@colorado.edu>
2022-03-15 18:40:40 -0600

    buildroot configuration for aesd-char driver

--- base_external/rootfs_overlay/etc/init.d/S98lddmodules.sh
+++ base_external/rootfs_overlay/etc/init.d/S98lddmodules.sh
@@ -6,12 +6,18 @@ case "$1" in
         modprobe hello
         /usr/bin/module_load faulty
         /usr/bin/scull_load
+
+        echo "Loading aesd character driver"
+        /usr/bin/aesdchar_load
         ;;
     stop)
         echo "Stopping scull and misc-modules"
         rmmod hello
         /usr/bin/module_unload faulty
         /usr/bin/scull_unload
+
+        echo "Stopping aesd character driver"
+        /usr/bin/aesdchar_unload
         ;;
     *)
     exit 1
