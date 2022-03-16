5dadb57f8884390e41c049572e1547b0ec0cb93a
ruchitnaik <ruchit.naik@colorado.edu>
2022-03-15 21:38:56 -0600

    bugfix: strchr replaced with for loop

--- base_external/package/aesd-char/aesd-char.mk
+++ base_external/package/aesd-char/aesd-char.mk
@@ -6,7 +6,7 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-AESD_CHAR_VERSION = 'bd52f872da2a31c11ef241107c702fa2d4d59d95'
+AESD_CHAR_VERSION = '62a3b95519fe18397ed89cd84b73db1c183a124d'
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
